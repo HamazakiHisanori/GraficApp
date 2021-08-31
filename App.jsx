e668d13374b964df7b2897caf5b196f6d439bac1
HamazakiHisanori <hiii1023@icloud.com>
2021-08-31 15:57:23 +0900

    修正

--- App.jsx
+++ App.jsx
@@ -1,22 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Text, StyleSheet, View, ImageBackground, TouchableOpacity, Animated,
+} from 'react-native';
+
+const image = require('./assets/snow.png');
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      {/* eslint-disable-next-line */}
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={image} style={styles.image}>
+
+      <View style={styles.container}>
+
+        <View style={styles.link}>
+          <TouchableOpacity style={styles.linkText}>
+            <Animated.Text style={styles.text}>sample</Animated.Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text>sample</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text>sample</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text>sample</Text>
+          </TouchableOpacity>
+        </View>
+
+      </View>
+
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  text: {
+    color: 'yellow',
+    fontSize: 48,
+    fontWeight: 'bold',
+    lineHeight: 32,
+    padding: 16,
   },
 });
